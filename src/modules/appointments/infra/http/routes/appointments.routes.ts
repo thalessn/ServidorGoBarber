f1d85dcd523a13c2478355de5bd496e663712663
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-10-07 22:30:22 -0300

    Aula: Usando Controllers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreatAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentController();
 
 // Habilita o middleware em todas as requisições
 appointmentsRouter.use(ensureAuthenticated);
@@ -17,19 +15,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //     return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointmentService = container.resolve(CreatAppointmentService);
-
-    const appointment = await createAppointmentService.execute({
-        provider_id,
-        date: parsedDate,
-    });
-
-    return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
