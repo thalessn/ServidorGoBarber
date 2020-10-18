bbe293baf46eac8766e01ba498bc78ef77626f85
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-10-18 16:41:25 -0300

    Aula: Testes de Agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -14,5 +15,26 @@ describe('CreateAppointment', () => {
         });
 
         expect(appointment).toHaveProperty('id');
+    });
+
+    it('shoud not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppoitment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppoitment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        });
+
+        expect(
+            createAppoitment.execute({
+                date: appointmentDate,
+                provider_id: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
