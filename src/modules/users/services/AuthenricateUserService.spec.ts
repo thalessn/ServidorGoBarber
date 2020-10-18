86182135b6defb54a7dbb4b5ff3b36f300a7712f
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-10-18 19:58:16 -0300

    Testes de autenticação

--- src/modules/users/services/AuthenricateUserService.spec.ts
+++ src/modules/users/services/AuthenricateUserService.spec.ts
@@ -31,4 +31,47 @@ describe('AuthenticateUser', () => {
 
         expect(response).toHaveProperty('token');
     });
+
+    it('Should be able to authenticate with non existing user', async () => {
+        const fakeUserRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@gmail.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('Should be able to authenticate with wrong password', async () => {
+        const fakeUserRepository = new FakeUserRepository();
+        const fakeHashProvider = new FakeHashProvider();
+        const createUser = new CreateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
+        const authenticateUser = new AuthenticateUserService(
+            fakeUserRepository,
+            fakeHashProvider,
+        );
+
+        await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            password: '123456',
+        });
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@gmail.com',
+                password: 'wrong-password',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
