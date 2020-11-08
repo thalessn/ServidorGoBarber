824511568f614c813409451fe6d3a04aef124157
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-11-07 21:57:03 -0300

    Aula: Emails em desenvolvimento

--- src/shared/infra/typeorm/migrations/1604783767156-CreateUserTokens.ts
+++ src/shared/infra/typeorm/migrations/1604783767156-CreateUserTokens.ts
@@ -5,7 +5,7 @@ export default class CreateUserTokens1604783767156
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'user_tokens',
+                name: 'users_tokens',
                 columns: [
                     {
                         name: 'id',
