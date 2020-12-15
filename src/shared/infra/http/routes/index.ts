af7a02bdda2a508c798de18a7ff4e5fbd5218715
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-12-14 23:30:53 -0300

    Aula: Listagem de Providers

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 // src/routes/index.ts
 import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '../../../../modules/users/infra/http/routes/password.routes';
@@ -10,6 +11,7 @@ const routes = Router();
 // Ele utilizou o "use" ao invés do verbos Http para passar para a rota appointmentsRouter
 // esse informação. Para consguir separa em arquivos diferentes as rotas.
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
