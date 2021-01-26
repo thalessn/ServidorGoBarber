93c2b0d5d03231afc1b2bef20c5875a8daee5e74
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-01-25 21:41:32 -0300

    Aula: Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import UpdateUserAvatarService from '../../../services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
@@ -16,8 +17,6 @@ export default class UserAvatarController {
             avatarFilename: request.file.filename,
         });
 
-        delete user.password;
-
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
